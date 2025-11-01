683dea82c28a3a803ea88c69d0f939d261d57d04
Braz D <brazdanarantes@gmail.com>
2025-11-01 09:25:54 -0300

    criação da classe Database.php

---
+++ src/config/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\config;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'controle_gastos';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    private function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO (
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4",
+                $this->username,
+                $this->password
+
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die(json_encode(["error" => "Erro na conexão com o banco de dados"]));
+        }
+
+        return $this->conn;
+
+    }
+
+
+}
